afa0c33ebb31e7001beb29cc977e76d9aa69ba0a
Rajeshrajes <rajeshraiser018@gmail.com>
2022-05-14 17:31:33 +0530

    commit chech

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React sathish
+          Learn React sathish rajesh
         
         </a>
       </header>
